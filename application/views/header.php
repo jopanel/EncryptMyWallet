00952621969cbfdac0690d25b4d7458aad05392b
Joseph F. Opanel IV <joe@DESIGNHBJ.local>
2017-10-11 14:23:12 -0700

    remove store
    removed store to create separate store in private repo.

--- application/views/header.php
+++ application/views/header.php
@@ -34,7 +34,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <a class="nav-link <?=navBarHelper($this->uri->segment(1),"")?>" href="<?=base_url()?>">Home</a>
                 <a class="nav-link <?=navBarHelper($this->uri->segment(1),"encrypt")?>" href="<?=base_url()?>encrypt">Encrypt</a>
                 <a class="nav-link <?=navBarHelper($this->uri->segment(1),"decrypt")?>" href="<?=base_url()?>decrypt">Decrypt</a>
-                <a class="nav-link <?=navBarHelper($this->uri->segment(1),"store")?>" href="<?=base_url()?>store">Buy An ID</a>
+                <a class="nav-link" href="https://store.encryptmywallet.com">Buy An ID</a>
                 <a class="nav-link <?=navBarHelper($this->uri->segment(1),"tos")?>" href="<?=base_url()?>tos">TOS</a>
               </nav>
             </div>
--- application/views/store/store.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-
-
-          <div class="inner cover">
-            <h1 class="cover-heading">Not Yet Available</h1>
-          </div>
